53099f79f249cb6869bd443f04b5219595da641c
SHUBRAT26 <shubrat.singh@yahoo.com>
2022-01-17 16:35:11 +0530

    test: Unit test case for home.js

--- src/Test/Home_test.js
+++ src/Test/Home_test.js
@@ -1,41 +1,67 @@
-import Home from '../Component/Home/Home';
-import {expect, fixture, html, unsafeStatic} from '@open-wc/testing';
+import Home from "../Component/Home/Home";
+import { expect, fixture, html, unsafeStatic } from "@open-wc/testing";
 
 const assert = chai.assert;
 
-suite('Header element test', () => {
-    test('Header element should be able to define', () => {
-        const el = document.createElement(`app-home`);
-        assert.instanceOf(el, Home);
+suite("Header element test", () => {
+  test("Header element should be able to define", () => {
+    const el = document.createElement(`app-home`);
+    assert.instanceOf(el, Home);
+  });
+
+  suite("Home element test", () => {
+    test("Home by default dialog visible to true", async () => {
+      const el = await fixture("<app-home></app-home>");
+      expect(el.dialogVisible).to.equal(true);
+    });
+  });
+
+  suite("Home element test", () => {
+    test('search input element should get the data', async () => {
+        const el = await fixture(html`<app-home></app-home>`);
+        const menu = el.shadowRoot.querySelector('#search_id');
+        expect(menu.nodeValue).to.equal(null)
     });
 
-    // test('header should have app drawer', async () => {
-    //     const el = await fixture(html`<header-element></header-element>`);
-    //     const sideBar = el.shadowRoot.querySelector('#drawer');
-    //     assert.instanceOf(sideBar, HTMLDivElement);
-    // });
+  })
 
-    // test('Delete specific data from after delete button click', async () => {
-    //     const el = await fixture(html`<app-home></app-home>`);
-    //     const deleteBtn = el.shadowRoot.querySelector('.delete-btn');
+  suite("Home element test", () => {
+    test('Edit button click open modal dialog', async () => {
+        const el = await fixture(html`<app-home></app-home>`);
+        const editBtn = el.shadowRoot.querySelector('.edit-btn');
         
-    //     deleteBtn.click();
-    //     await el.updateComplete;
-    // });
+        editBtn.click();
+        expect(el.dialogVisible).to.equal(false)
+    });
 
-    // test('side navigation drawer should close', async () => {
-    //     const el = await fixture(html`<header-element></header-element>`);
-    //     const menu = el.shadowRoot.querySelector('.menu');
-        
-    //     menu.click();
-    //     await el.updateComplete;
+  })
 
-    //     const closeBtn = el.shadowRoot.querySelector('.closebtn');
-    //     closeBtn.click();
-    //     await el.updateComplete;
+  // test('has a sideBar', async () => {
+  //     const el = await fixture(html`<app-home></app-home>`);
+  //     const sideBar = el.shadowRoot.getElementByClassName('.bookLanding__homepage');
+  //     const sideBar2 = sideBar.getElementByTagName('side-bar')
+  // });
 
-    //     const sideBar = el.shadowRoot.querySelector('#drawer');
-    //     assert.equal(sideBar.style.width, "0px");
-    // });
-  
+  // test('Delete specific data from after delete button click', async () => {
+  //     const el = await fixture(html`<app-home></app-home>`);
+  //     const deleteBtn = el.shadowRoot.querySelector('.delete-btn');
+
+  //     deleteBtn.click();
+  //     await el.updateComplete;
+  // });
+
+  // test('side navigation drawer should close', async () => {
+  //     const el = await fixture(html`<header-element></header-element>`);
+  //     const menu = el.shadowRoot.querySelector('.menu');
+
+  //     menu.click();
+  //     await el.updateComplete;
+
+  //     const closeBtn = el.shadowRoot.querySelector('.closebtn');
+  //     closeBtn.click();
+  //     await el.updateComplete;
+
+  //     const sideBar = el.shadowRoot.querySelector('#drawer');
+  //     assert.equal(sideBar.style.width, "0px");
+  // });
 });
